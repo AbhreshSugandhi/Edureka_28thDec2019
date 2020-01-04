ebecfac524e6d543e7b26cf775fdf75bd13971ca
Abhresh <tabhresh@gmail.com>
2020-01-04 23:23:26 +0530

    04012020

--- src/module2/Facebook.java
+++ src/module2/Facebook.java
@@ -1,19 +1,31 @@
 package module2;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Facebook {
 
 	public static void main(String[] args) {
 		
 		//Open Chrome Browser
-				
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhre\\Documents\\GSync\\PCSync\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+						
 		//Open AUT
+		driver.get("https://www.facebook.com/");
 				
 		//Interact with my elements on FB AUT using various Locator options
 			//ID
+			driver.findElement(By.id("u_0_m")).sendKeys("Abhresh");
 		
 			//Name
+			driver.findElement(By.name("lastname")).sendKeys("Sugandhi");
 			
 			//ClassName
+			driver.findElement(By.className("inputtext login_form_input_box")).sendKeys("Abhresh");
+			
+			driver.findElement(By.className("inputtext login_form_input_box")).sendKeys("Sugandhi");
 			
 			//LinkText
 			
